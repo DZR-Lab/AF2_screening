e33d4f61502290efc7f908f2ed96d781cd624512
jlwarner <james_warner@hms.harvard.edu>
2026-07-20 14:54:17 -0400

    Sbatch submit edits
    Removed my email and replaced with placeholder

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -8,14 +8,14 @@
 #SBATCH -o logs/jobs/%j.out                            # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e logs/jobs/%j.err                            # File to which STDERR will be written, including job ID (%j)
 #SBATCH --mail-type=ALL                      # ALL email notification type
-#SBATCH --mail-user=<james_warner@hms.harvard.edu>          # Email to which notifications will be sent
+#SBATCH --mail-user=<YOUR_EMAIL_HERE>          # Email to which notifications will be sent
 
 module load conda/miniforge3/24.11.3-0
 
 conda activate af2-multimer-analysis
 
-cd /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/analysis/
+cd analysis/
 
-python3 /home/jw362/AF2multimer-analysis/colabfold_analysis.py /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_1 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_2 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_3 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_4 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_5 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_6 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_7 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_8 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_9 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_10 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_11 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_12 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_13 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_14 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_15 /n/data1/hms/microbiology/rudner/lab/jw362/colabfold/SaPBP2/output/part_16  --pae 12 --plddt 50 --pae-mode avg --combine-all 
+python3 /home/jw362/AF2multimer-analysis/colabfold_analysis.py ../output/part_1 ../output/part_2 ../output/part_3 ../output/part_4 ../output/part_5 ../output/part_6 ../output/part_7 ../output/part_8 ../output/part_9 ../output/part_10 ../output/part_11 ../output/part_12 ../output/part_13 ../output/part_14 ../output/part_15 ../output/part_16  --pae 12 --plddt 50 --pae-mode avg --combine-all 
 
 conda deactivate
--- scripts/make_repeat_inputs.sh
+++ scripts/make_repeat_inputs.sh
@@ -8,7 +8,7 @@
 #SBATCH -o logs/jobs/%j.out                            # File to which STDOUT will be written, including job ID (%j)
 #SBATCH -e logs/jobs/%j.err                            # File to which STDERR will be written, including job ID (%j)
 #SBATCH --mail-type=ALL                      # ALL email notification type
-#SBATCH --mail-user=<james_warner@hms.harvard.edu>          # Email to which notifications will be sent
+#SBATCH --mail-user=<YOUR_EMAIL_HERE>          # Email to which notifications will be sent
 
 
 cp -r input/split_inputs input/repeat_inputs
